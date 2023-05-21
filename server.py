17846ce65eed498c02396599f3e67c2890fcb1e3
chandan <chandanp20k@gmail.com>
2023-05-21 17:14:02 +0530

    Initial commit

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# @app.route('/hai/<username>/<int:post_id>')
+# def hello_world(username = None, post_id = None):
+#     return render_template('index.html', name= username, post_id = post_id)
+
+@app.route('/')
+def portpolio():
+    return render_template('index.html')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
+@app.route('/works.html')
+def works():
+    return render_template('works.html')
+
+@app.route('/work.html')
+def work():
+    return render_template('work.html')
+
+@app.route('/contact.html')
+def contacts():
+    return render_template('contact.html')
+
+# @app.route('/hello')
+# def index():
+#     return render_template('index.html')
